52ed1a1229a903590fd586f24b1417cf64472ca4
Fernando Mazzon <fer662@gmail.com>
2014-02-11 11:50:48 -0300

    Made observed view retain FMVisibilityObserverConstraintSwitch, so that an orphan FMVisibilityObserverConstraintSwitch in a nib file or storyboard wouldn't get released if it was observing something, even without strong outlets. Unhardcoded constraint priorities into constants.

--- Classes/FMVisibilityObserverConstraintSwitch.m
+++ Classes/FMVisibilityObserverConstraintSwitch.m
@@ -7,22 +7,39 @@
 //
 
 #import "FMVisibilityObserverConstraintSwitch.h"
+#import <objc/runtime.h>
+
+static UILayoutPriority kRequiredLayoutPriority = 999;
+static UILayoutPriority kNotRequiredLayoutPriority = 1;
+
+static char kObservedViewRetainKey;
 
 @implementation FMVisibilityObserverConstraintSwitch
 
 - (void)setObservedView:(UIView *)observedView
 {
-    [self.observedView removeObserver:self forKeyPath:@"hidden" context:nil];
+    if (_observedView) {
+        //Make the old observed view release this switch
+        objc_setAssociatedObject(_observedView, &kObservedViewRetainKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        [_observedView removeObserver:self forKeyPath:@"hidden" context:nil];
+    }
+    _observedView = observedView;
+    if (_observedView) {
+        //Make the new observed view retain this switch. This is so this object doesn't need an strong outlet
+        //to prevent it from getting released.
+        objc_setAssociatedObject(_observedView, &kObservedViewRetainKey, self, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    
     [observedView addObserver:self forKeyPath:@"hidden" options:0 context:nil];
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(UIView *)object change:(NSDictionary *)change context:(void *)context
 {
     for (NSLayoutConstraint *c in self.hiddenConstraints) {
-        c.priority = [object isHidden]?999:1;
+        c.priority = [object isHidden]?kRequiredLayoutPriority:kNotRequiredLayoutPriority;
     }
     for (NSLayoutConstraint *c in self.visibleConstraints) {
-        c.priority = [object isHidden]?1:999;
+        c.priority = [object isHidden]?kNotRequiredLayoutPriority:kRequiredLayoutPriority;
     }
     if (self.animated) {
         [UIView animateWithDuration:self.animationDuration delay:0 options:self.animationCurve << 16 animations:^{
