3ae6a0fb2b12057e2eb80c898b009ed386feff8f
Fernando Mazzon <fer662@gmail.com>
2014-07-10 16:35:52 -0300

    validating that we actually need to perform any changes Updated pod spec

--- Classes/FMStateConstraintSwitch.m
+++ Classes/FMStateConstraintSwitch.m
@@ -12,23 +12,25 @@
 
 - (void)setState:(BOOL)state
 {
-    _state = state;
-    
-    for (NSLayoutConstraint *c in self.noStateConstraints) {
-        c.priority = state?1:999;
-    }
-    for (NSLayoutConstraint *c in self.yesStateConstraints) {
-        c.priority = state?999:1;
-    }
-    
-    if (self.animated) {
-        [UIView animateWithDuration:self.animationDuration delay:0 options:self.animationCurve << 16 animations:^{
+    if (_state != state) {
+        _state = state;
+        
+        for (NSLayoutConstraint *c in self.noStateConstraints) {
+            c.priority = state?1:999;
+        }
+        for (NSLayoutConstraint *c in self.yesStateConstraints) {
+            c.priority = state?999:1;
+        }
+        
+        if (self.animated) {
+            [UIView animateWithDuration:self.animationDuration delay:0 options:self.animationCurve << 16 animations:^{
+                [self.layoutedView layoutIfNeeded];
+            } completion:^(BOOL finished) {
+            }];
+        }
+        else {
             [self.layoutedView layoutIfNeeded];
-        } completion:^(BOOL finished) {
-        }];
-    }
-    else {
-        [self.layoutedView layoutIfNeeded];
+        }
     }
 }
 
